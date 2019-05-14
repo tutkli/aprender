8327b1321daf37278eef2644e46b0451152e7162
Clara Castillo <clarakiba@gmail.com>
2019-05-14 21:03:59 +0200

    Cambio de Splash Screen a AsyncTask

--- app/src/main/java/com/example/clara/aprender/SplashScreen.java
+++ app/src/main/java/com/example/clara/aprender/SplashScreen.java
@@ -1,9 +1,12 @@
 package com.example.clara.aprender;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ProgressBar;
 
 import com.felipecsl.gifimageview.library.GifImageView;
@@ -12,11 +15,14 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
+
+import static android.view.View.VISIBLE;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private GifImageView gifImageView;
-    private ProgressBar progressBar;
+    GifImageView gifImageView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +31,50 @@ public class SplashScreen extends AppCompatActivity {
 
         gifImageView = (GifImageView) findViewById(R.id.gifImageView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setVisibility(progressBar.VISIBLE);
+        progressBar.setVisibility(VISIBLE);
 
-        //Set GIFImageView resource
         try {
             InputStream inputStream = getAssets().open("splash_bg.gif");
             byte[] bytes = IOUtils.toByteArray(inputStream);
             gifImageView.setBytes(bytes);
             gifImageView.startAnimation();
         } catch (IOException ex) {
-
+            Log.d("SplashScreen: ", "ERROR");
         }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                SplashScreen.this.startActivity(new Intent(SplashScreen.this, MainActivity.class));
-                SplashScreen.this.finish();
-            }
-        }, 3000);
+        new Cargar(this).execute();
+    }
 
+    public static class Cargar extends AsyncTask<Void, Void, Boolean> {
+
+        private final WeakReference<Activity> weakReference;
+
+        private Cargar(Activity activity) {
+            this.weakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                Thread.sleep(3000);
+            }catch(InterruptedException e) {
+                Log.d("SplashActivity:", "ERROR");
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            super.onPostExecute(bool);
+            Activity activity = weakReference.get();
+            Intent i = new Intent(activity, MainActivity.class);
+            activity.startActivity(i);
+            activity.finish();
+        }
     }
 }
