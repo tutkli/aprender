7aadd92d6db711b97d3b2baefa622a555857484d
raulcruzsouto <50080015+raulcruzsouto@users.noreply.github.com>
2019-05-23 15:53:15 +0200

    d

--- app/src/main/java/com/example/clara/aprender/ConfiguracionActivity.java
+++ app/src/main/java/com/example/clara/aprender/ConfiguracionActivity.java
@@ -1,5 +1,9 @@
 package com.example.clara.aprender;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +11,13 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
-public class ConfiguracionActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class ConfiguracionActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,5 +71,40 @@ public class ConfiguracionActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+    public void cerrarsesion(View v){
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        startActivity(new Intent(ConfiguracionActivity.this,Login.class));
+    }
+    public void borraruser(View v){
+        AlertDialog.Builder dialog   = new AlertDialog.Builder(ConfiguracionActivity.this);
+        dialog.setTitle("Estas seguro?");
+        dialog.setMessage("Si acepta su cuenta sera completamente borrada y perdera todos los datos");
+        dialog.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                currentUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(ConfiguracionActivity.this,"cuenta eliminada",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(ConfiguracionActivity.this,Login.class));
+                        }else{
+                            Toast.makeText(ConfiguracionActivity.this,"error al eliminar el usuario",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+        dialog.setNegativeButton("cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
     }
 }
