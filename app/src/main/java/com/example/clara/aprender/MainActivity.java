7bf9f98f0da9e62495f03db86009ed676a8b2b56
Clara Castillo <claracastillochale@gmail.com>
2019-05-23 20:40:04 +0200

    gitignore fix

--- app/src/main/java/com/example/clara/aprender/MainActivity.java
+++ app/src/main/java/com/example/clara/aprender/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, MenuNivelActivity.class);
+                Intent i = new Intent(MainActivity.this, Login.class);
                 startActivity(i);
             }
         });
