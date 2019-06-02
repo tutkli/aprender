7807ea447e0a8727df194c8be5c0d036e0f0a6bf
Aleksander <alemilab@gmail.com>
2019-06-02 13:58:46 +0200

    Boton de Musica cambia de icono caundo se le da para ofrecer feedback

--- app/src/main/java/com/example/clara/aprender/Juego.java
+++ app/src/main/java/com/example/clara/aprender/Juego.java
@@ -26,7 +26,8 @@ public class Juego extends AppCompatActivity {
     TextView Input, Output;
     boolean EstadoMusica;
     MediaPlayer musica;
-
+    Nivel nivel_actual;
+    boolean juego_start;
 
 
     @Override
@@ -36,6 +37,7 @@ public class Juego extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
         EstadoMusica=true;
+        juego_start=false;
         init();
 
         if (savedInstanceState == null) {
@@ -45,8 +47,8 @@ public class Juego extends AppCompatActivity {
 
     }
 
-
     private void showFragment(Fragment fragment) {
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment, "fragment").commit();
     }
@@ -80,16 +82,21 @@ public class Juego extends AppCompatActivity {
     }
     // Poner un Android Tour? para explicar los diferentes items en la lista
     private void Ayuda(){
-
+        //Mostrar un cardview con
     }
     //Ir cada 0.5 segundos una instruccion hacia adelante
     //Tiene que coger los elementos de la columna 2, y lo tiene que comparar con una solucion?
     private void Jugar(){
+        // Recorrer la lista
+        // Poner un switch, que según el elemento de la lista, haga distintas cosas.
 
     }
     //Cuando esta ejecutandose, el boton de play se cambia a parar, donde podemos parar el juego, el index de instrucciones se reinicia y se cambia el boton a play
     private void Parar(){
-
+        // Poner el estado de restart
+        // Cambiar todos los elementos a color normal
+        // Reiniciar el contador
+        // Reiniciar las animaciones
     }
     //Mover hacia atrás en la lista de elementos
     private void Atras(){
@@ -106,6 +113,7 @@ public class Juego extends AppCompatActivity {
         musica = MediaPlayer.create(getApplicationContext(), R.raw.musica);
         musica.setLooping(true);
         musica.start();
+
         IBSonido = findViewById(R.id.IBSonido);
         IBSonido.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,16 +121,106 @@ public class Juego extends AppCompatActivity {
                 Sonido();
             }
         });
+        IBAyuda = findViewById(R.id.IBAyuda);
+        IBAyuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Ayuda();
+            }
+        });
+        IBPlay = findViewById(R.id.IBPlay);
+        IBPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(juego_start){
+                    juego_start=true;
+                    Jugar();
+                }else{
+                    juego_start=false;
+                    Parar();
+                }
+
+            }
+        });
+        IBAdelante = findViewById(R.id.IBAdelante);
+        IBAdelante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Adelante();
+            }
+        });
+        IBAtras = findViewById(R.id.IBAtras);
+        IBAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Atras();
+            }
+        });
+
 
         // Con el id que le pasamos desde la parte de niveles, hacemos una busqueda que cargaremos en la pantalla.
         int id_nivel = getIntent().getExtras().getInt("id");
         Base_datos_Aprender BDAprender = Room.databaseBuilder(getApplicationContext(), Base_datos_Aprender.class, "base_datos_aprender").allowMainThreadQueries().build();
-        Nivel nivel_actual = BDAprender.getNivelDAO().getNivelPorID(id_nivel);
+        nivel_actual = BDAprender.getNivelDAO().getNivelPorID(id_nivel);
         // De momento se pone en un textView,
         Input_ini = nivel_actual.getInput();
         Input.setText(Input_ini);
         Output_ini = nivel_actual.getOutput();
         Output.setText(Output_ini);
+    }
+
+
+
+
+    // Métodos del juego.
+    // El objeto es un cuadro.
+    // Inicio: es el estado por defecto, a lo que vuelve la caja.
+
+    public void Input(){
+
+    }
+    // Mueve la caja de inicio, que necesita un valor dentro y lo mueve a la parte de output, y elimina el valor de la caja, y vuelve al inicio
+    public void Output(){
+
+    }
+    // Suma 1 al valor del cuadro, y le hace zoom (al cuadro) por unos milisegundos, para mostrar que está ocurriendo algo.
+    public void BumpMas(){
+
+    }
+    // Lo mismo pero le quita 1
+    public void BumpMenos(){
+
+    }
+    // mueve el objeto a uno de los holders, y lo rellena con el cuadro y el valor (cambia el fondo del cuadrado.), y vuelve a la posición inicial
+    public void copyTo(int holder){
+
+    }
+    // mueve el objeto al holder y cambia el valor del objeto al contenido del holder, y vuelve a la posición inicio
+    public void copyFrom(int holder){
+
+    }
+    // Coge el objeto de la zona de inicio, lo lleva a uno de las posiciones de los holders, y le añade realiza la suma
+    public void Sum(int holder){
+
+    }
+    // El objeto de la zona de inicio, va al holder marcado, hace la resta, el resultado se queda en el objeto y vuelve al inicio
+    public void Sub(int holder){
+
+    }
+    public void Jump(String Tipo){
+        // Diferentes Jump
+        //Jump
+        //Jump if zero
+        //Jump if negative
+        //Jump if not number
+    }
+    // Si mientras se ejecuta, ocurre algún error en la ejecución, con las reglas, te lo mostraría con este método, hace que ele elemento se ponga en rojo en la lista.
+    public void ErrorEnEjecución(){
+
+    }
+    // Muestra la calificación, en un cardview y la guarda en la base de datos.
+    public void Victoria(){
+
     }
 
 }
