ffca64c97dca8c6f0b2db26531e34c6a7a19653a
Aleksander <alemilab@gmail.com>
2019-06-02 11:59:08 +0200

    Boton de música en el juego funciona, falta hacer que cambie el botón

--- app/src/main/java/com/example/clara/aprender/Juego.java
+++ app/src/main/java/com/example/clara/aprender/Juego.java
@@ -7,9 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.clara.aprender.Base_datos.Base_datos_Aprender;
 import com.example.clara.aprender.Lista_Juego.BoardFragment;
@@ -18,22 +21,28 @@ import com.example.clara.aprender.Modelos.Nivel;
 public class Juego extends AppCompatActivity {
 
     ImageButton IBSonido, IBAyuda, IBAtras, IBPlay, IBAdelante;
+    String Input_ini;
+    String Output_ini;
+    TextView Input, Output;
+    boolean EstadoMusica;
+    MediaPlayer musica;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
-
-        int id_nivel = getIntent().getExtras().getInt("id");
-        Base_datos_Aprender BDAprender = Room.databaseBuilder(getApplicationContext(), Base_datos_Aprender.class, "base_datos_aprender").allowMainThreadQueries().build();
-        Nivel nivel_actual = BDAprender.getNivelDAO().getNivelPorID(id_nivel);
-        //Hay que añadir limitadores de elementos etc.
+        EstadoMusica=true;
+        init();
 
         if (savedInstanceState == null) {
             showFragment(BoardFragment.newInstance());
         }
+
+
     }
 
 
@@ -55,14 +64,30 @@ public class Juego extends AppCompatActivity {
     //Cada uno de estos corresponde a los botones.
     //Poner un cambio de sonido a muteado en la imagen, y poner cierto sonido cuando pulsamos play, aunque sea como test
     private void Sonido(){
+        Toast toast;
 
+        if(EstadoMusica) {
+            //Para silenciar la canción
+            EstadoMusica = false;
+            musica.pause();
+
+
+
+
+            toast=Toast.makeText(getApplicationContext(),"La cancion se ha parado",Toast.LENGTH_SHORT);
+        }else{
+            int length = musica.getCurrentPosition();
+            musica.seekTo(length);
+            musica.start();
+            EstadoMusica= true;
+            toast=Toast.makeText(getApplicationContext(),"La cancion ha vuelto",Toast.LENGTH_SHORT);
+
+        }
+        toast.show();
+        // Mirar como hacer esto -> IBSonido.setImageResource(R.drawable.ic_lock_silent_mode_off);
     }
     // Poner un Android Tour? para explicar los diferentes items en la lista
     private void Ayuda(){
-
-    }
-    //Mover hacia atrás en la lista de elementos
-    private void Atras(){
 
     }
     //Ir cada 0.5 segundos una instruccion hacia adelante
@@ -74,9 +99,38 @@ public class Juego extends AppCompatActivity {
     private void Parar(){
 
     }
+    //Mover hacia atrás en la lista de elementos
+    private void Atras(){
+
+    }
     //Mover hacia adelante en la lista de elementos
     private void Adelante(){
 
+    }
+
+    private void init(){
+        Input = findViewById(R.id.Input);
+        Output = findViewById(R.id.Output);
+        musica = MediaPlayer.create(getApplicationContext(), R.raw.musica);
+        musica.setLooping(true);
+        musica.start();
+        IBSonido = findViewById(R.id.IBSonido);
+        IBSonido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Sonido();
+            }
+        });
+
+        // Con el id que le pasamos desde la parte de niveles, hacemos una busqueda que cargaremos en la pantalla.
+        int id_nivel = getIntent().getExtras().getInt("id");
+        Base_datos_Aprender BDAprender = Room.databaseBuilder(getApplicationContext(), Base_datos_Aprender.class, "base_datos_aprender").allowMainThreadQueries().build();
+        Nivel nivel_actual = BDAprender.getNivelDAO().getNivelPorID(id_nivel);
+        // De momento se pone en un textView,
+        Input_ini = nivel_actual.getInput();
+        Input.setText(Input_ini);
+        Output_ini = nivel_actual.getOutput();
+        Output.setText(Output_ini);
     }
 
 }
